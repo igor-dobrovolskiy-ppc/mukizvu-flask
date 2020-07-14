95322ee7d71273c7d515997d817fc4f2ac7f299a
Ihor Dobrovolskyi <ihor.dobrovolskyi@consultant.lego.com>
2020-07-14 13:33:59 +0300

    Added personnel assets and carousel at home page

--- gen_test_data.py
+++ gen_test_data.py
@@ -13,18 +13,18 @@ db.session.commit()
 
 db.session.query(Person).delete()
 
-person = Person(name="Adam", surname="First")
-db.session.add(person)
-person2 = Person(name="Sergei aka 'Platon' aka 'Beard'", surname="Distriyanov")
+person2 = Person(name="Sergei", surname="aka 'Platon' aka 'Beard' Distriyanov")
 db.session.add(person2)
 person3 = Person(name="Vitalii", surname="Melnik")
 db.session.add(person3)
-person4 = Person(name="Ihor aka 'INgW4R'", surname="Dobrovolskyi")
+person4 = Person(name="Ihor", surname="aka 'INgW4R' Dobrovolskyi")
 db.session.add(person4)
-person5= Person(name="Maxim", surname="Voronko")
+person5 = Person(name="Maxim", surname="Voronko")
 db.session.add(person5)
-person6= Person(name="Alex aka 'Travis'", surname="")
+person6 = Person(name="Alex", surname="aka 'Travis'")
 db.session.add(person6)
+person7 = Person(name="Valentina", surname="Taisija (Mamchyna)'")
+db.session.add(person7)
 db.session.commit()
 
 db.session.query(Author).delete()
@@ -46,26 +46,28 @@ performer4 = Performer(person_id=person5.id)
 db.session.add(performer4)
 performer5 = Performer(person_id=person6.id)
 db.session.add(performer5)
+performer6 = Performer(person_id=person7.id)
+db.session.add(performer6)
 db.session.commit()
 
 db.session.query(Opus).delete()
 
-opus = Opus(name="Imagine")
+opus = Opus(name="Mole")
 opus.authors = [author]
 db.session.add(opus)
-opus2 = Opus(name="Help!")
+opus2 = Opus(name="Sharik")
 opus2.authors = [author, author2]
 db.session.commit()
 
 db.session.query(Band).delete()
 
-band = Band.from_name("The Beatles")
+band = Band.from_name("Muki Zvu")
 db.session.add(band)
 db.session.commit()
 
 db.session.query(Personnel).delete()
 
-personnel = Personnel(band.id, start_date=datetime.date(1960, 8, 15), end_date=datetime.date(1970, 4, 10))
+personnel = Personnel(band.id, start_date=datetime.date(2005, 6, 5))
 db.session.add(personnel)
 db.session.commit()
 
@@ -93,7 +95,6 @@ for a in acs:
     db.session.add(a)
 db.session.commit()
 
-
 s = personnel_performer_roles.insert().values([
     (performer.id, personnel.id, role.id),
     (performer2.id, personnel.id, role2.id)
@@ -115,13 +116,13 @@ db.session.commit()
 
 db.session.query(PerformancePlace).delete()
 
-place = PerformancePlace(name="Abbyy Road")
+place = PerformancePlace(name="NIPIASUtransgas")
 db.session.add(place)
 db.session.commit()
 
 db.session.query(PerformanceEvent).delete()
 
 event = PerformanceEvent(perf_place_id=place.id, personnel_id=personnel.id,
-                         date=datetime.date(year=1069, month=5, day=12), name="Abbyy Road album recording")
+                         date=datetime.date(year=2008, month=3, day=8), name="8th of March Celebration, 2008")
 db.session.add(event)
 db.session.commit()
